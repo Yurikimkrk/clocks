64b7be216039637ff595135d28bd97c5cd59f541
yuri <yurikimkrk@gmail.com>
2021-08-22 22:25:36 +0700

    v2.2

--- src/requests/requests.js
+++ src/requests/requests.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const timezonesRequest = async() => {
   const instance = await axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: (process.env.NODE_ENV === 'development') ? 'http://localhost:5000': 'http://yuriykimtest.tk'
   })
   return instance.get(`/api/city`).then(response => (response.data))
 }
